8dc88dea6e6f27e2bb9f3d5ed33d8931aa5b9fac
pawitchaya <boss@cmu.ac.th>
2025-01-30 23:13:45 +0700

    lab15_4.cpp completed

--- lab15_4.cpp
+++ lab15_4.cpp
@@ -17,5 +17,26 @@ int main(){
 	
 	return 0;
 }
+void shuffle(int &a, int &b, int &c, int &d) {
+    int random1 = rand() % 4;
+    int random2 = rand() % 4;
+    
+    while (random1 == random2) {
+        random2 = rand() % 4;
+    }
+
+    if (random1 == 0 && random2 == 1) swap(a, b);
+    else if (random1 == 0 && random2 == 2) swap(a, c);
+    else if (random1 == 0 && random2 == 3) swap(a, d);
+    else if (random1 == 1 && random2 == 2) swap(b, c);
+    else if (random1 == 1 && random2 == 3) swap(b, d);
+    else if (random1 == 2 && random2 == 3) swap(c, d);
+    else if (random1 == 1 && random2 == 0) swap(b, a);
+    else if (random1 == 2 && random2 == 0) swap(c, a);
+    else if (random1 == 3 && random2 == 0) swap(d, a);
+    else if (random1 == 2 && random2 == 1) swap(c, b);
+    else if (random1 == 3 && random2 == 1) swap(d, b);
+    else if (random1 == 3 && random2 == 2) swap(d, c);
+}
 
 //Do not modify source code above this line
